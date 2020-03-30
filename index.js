05e1c68e89ac4928187261f54a67d1b66c8cb71a
ludanxer <ludanxer@gmail.com>
2020-03-30 13:42:51 +0800

    rewrite base on github template repo

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-#!/usr/bin/env node
+const yParser = require('yargs-parser')
+const semverSatisfies = require('semver/functions/satisfies')
+const chalk = require('chalk')
+const shell = require('shelljs')
+const query = require('./lib/query')
+const { name, version } = require('./package.json')
+const debug = require('debug')
 
-const yParser = require('yargs-parser');
-const semver = require('semver');
-const chalk = require('chalk');
-const run = require('./lib/run');
-const { name, version } = require('./package');
-const debug = require('debug');
+const args = yParser(process.argv.slice(2))
 
-// Get cli arguments
-const args = yParser(process.argv.slice(2));
-
-// Output version with -v, --version flag
 if (args.v || args.version) {
-  console.log(name, version);
-  process.exit(0);
+  console.log(name, version)
+  process.exit(0)
 }
 
-// Enalbe debug mode with -d, --debug flag
 if (args.d || args.debug) {
-  debug.enable('create-vuepress:*');
+  debug.enable('create-vuepress:*')
 }
 
-if (!semver.satisfies(process.version, '>= 8.0.0')) {
-  console.error(chalk.red('✘ The generator will only work with Node v8.0.0 and up!'));
-  process.exit(1);
+if (!semverSatisfies(process.version, '>= 8.0.0')) {
+  console.error(chalk.red('✘ Generator only works with Node v8.0.0 and up!'))
+  process.exit(1)
 }
 
-const targetDir = args._[0] || '';
-run({
-  targetDir,
-  args
-});
+if (!shell.which('git')) {
+  console.error(chalk.red('✘ Generator needs git to work!'))
+  process.exit(1)
+}
+
+query({
+  targetDir: args._[0] || '.',
+  debug: args.d || args.debug || false,
+  remove: args.remove || false,
+})
